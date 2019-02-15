89b2a487204681ac7cc933e7857db39644b8166f
AlinaRodina99 <alnardn207@gmail.com>
2019-02-16 02:50:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Result: {Factorial(7)} ");
+            Console.ReadKey();
+        }
+        static int Factorial(int n)
+        {
+            if (n<=1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+}
