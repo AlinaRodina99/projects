55899b13b2283792eeb5c7fb6e2843b67e2b2b2e
AlinaRodina99 <alnardn207@gmail.com>
2019-03-14 02:06:49 +0300

    added task 3.1

---
+++ hometask3/3.1/TestsStackOnList.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+using StackCalculator;
+
+namespace Tests
+{
+    public class TestsStackOnList
+    {
+        private StackOnList<double> stack;
+        [SetUp]
+        public void Setup()
+        {
+            stack = new StackOnList<double>();
+        }
+
+        [Test]
+        public void PushTest()
+        {
+            stack.Push(3);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        [Test]
+        public void PopTest()
+        {
+            stack.Push(4);
+            Assert.AreEqual(4, stack.Pop());
+        }
+
+        [Test]
+        public void PeekTest()
+        {
+            stack.Push(4);
+            Assert.AreEqual(4, stack.Peek());
+        }
+
+        [Test]
+        public void PopFfromEmptyStack()
+        {
+            stack.Pop();
+        }
+
+        [Test]
+        public void PeekFormEmptyStack()
+        {
+            stack.Peek();
+        }
+
+        [Test]
+        public void TwoElementsPopTest()
+        {
+            stack.Push(3);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(3, stack.Pop());
+        }
+
+        [Test]
+        public void ManyPushTest()
+        {
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            stack.Push(7);
+            stack.Push(8);
+            stack.Push(9);
+            Assert.AreEqual(9, stack.Pop());
+            Assert.AreEqual(8, stack.Pop());
+            Assert.AreEqual(7, stack.Pop());
+            Assert.AreEqual(6, stack.Pop());
+            Assert.AreEqual(5, stack.Pop());
+            Assert.AreEqual(4, stack.Pop());
+        }
+
+        [Test]
+        public void PushAndPopElements()
+        {
+            stack.Push(3);
+            stack.Push(2);
+            stack.Pop();
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+    }
+}
