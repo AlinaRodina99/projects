70c737deaf322c60d72b086963b0ffaba3d8d8f6
AlinaRodina99 <alnardn207@gmail.com>
2019-03-29 22:34:17 +0300

    added task 2.3

---
+++ hometask2/2.3/StackCalculator/ElementOfList.cs
@@ -0,0 +1,12 @@
+﻿namespace StackCalculator
+{
+    public class ElementOfList<T>
+    {
+        public ElementOfList(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public ElementOfList<T> Next { get; set; }
+    }
+}
---
+++ hometask2/2.3/StackCalculator/StackOnList.cs
@@ -0,0 +1,64 @@
+﻿namespace StackCalculator
+{
+    public class StackOnList<T> : IStack<T>
+    {
+        ElementOfList<T> head;
+        int size;
+        public StackOnList()
+        {
+            size = 0;
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return size == 0;
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+
+        public void Push(T data)
+        {
+            ElementOfList<T> newElement = new ElementOfList<T>(data);
+            newElement.Next = head;
+            head = newElement;
+            size++;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty == true)
+            {
+                return default(T);
+            }
+            else
+            {
+                ElementOfList<T> temp = head;
+                head = head.Next;
+                size--;
+                return temp.Data;
+            }
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty == true)
+            {
+                return default(T);
+            }
+            else
+            {
+                return head.Data;
+            }
+        }
+    }
+}
+
