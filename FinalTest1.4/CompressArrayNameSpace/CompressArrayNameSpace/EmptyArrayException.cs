ba3d47796a8bb732abe99f4a01bdb5737faf87ef
AlinaRodina99 <alnardn207@gmail.com>
2019-05-31 15:11:29 +0300

    added final test

---
+++ FinalTest1.4/CompressArrayNameSpace/CompressArrayNameSpace/EmptyArrayException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CompressArrayNameSpace
+{
+    /// <summary>
+    /// Class for exception when array is empty.
+    /// </summary>
+    public class EmptyArrayException : Exception
+    {
+        /// <summary>
+        /// Constructor fot exception.
+        /// </summary>
+        /// <param name="message">Message about empty.</param>
+        public EmptyArrayException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+}
