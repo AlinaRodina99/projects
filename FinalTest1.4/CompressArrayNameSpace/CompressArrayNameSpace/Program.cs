2a561bd0abb9fced10f73d2e38fe490b1d285d5f
AlinaRodina99 <alnardn207@gmail.com>
2019-05-31 15:18:51 +0300

    forgot space

--- FinalTest1.4/CompressArrayNameSpace/CompressArrayNameSpace/Program.cs
+++ FinalTest1.4/CompressArrayNameSpace/CompressArrayNameSpace/Program.cs
@@ -9,7 +9,7 @@ namespace CompressArrayNameSpace
             var array = new byte[4] { 1, 2, 2, 3 };
             CompressionOfArray compressionOf = new CompressionOfArray();
             var newArr = compressionOf.Compression(array);
-            foreach(var i in newArr)
+            foreach (var i in newArr)
             {
                 Console.WriteLine(i);
             }
