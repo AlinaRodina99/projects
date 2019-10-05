e5bd855de877e03d4c5389001777196a3693f255
unknown <alnardn207@gmail.com>
2019-10-05 20:31:57 +0300

    changed tests

--- homework.sem3/task1/Lazy_sem3.task1/TestsOfLazy/TestsOneThreadLazy.cs
+++ homework.sem3/task1/Lazy_sem3.task1/TestsOfLazy/TestsOneThreadLazy.cs
@@ -45,9 +45,43 @@ namespace Tests
         }
 
         [Test]
-        public void AreObjectsSameTest()
+        public void AreObjectsSameWithOneThreadLazyTest()
         {
+            var func = new Func<string>(() => "Test");
+            var lazy = LazyFactory<string>.CreateOneThreadLazy(func);
+            var result = lazy.Get();
+            Assert.IsTrue(result.Equals(lazy.Get()));
+            Assert.IsTrue(result.Equals(lazy.Get()));
+        }
 
+        [Test]
+        public void AreObjectsSameWithMultiThreadedLazyTest()
+        {
+            const int n = 5;
+            var func = new Func<int>(() => 5 * 2);
+            var lazy = LazyFactory<int>.CreateMultiThreadLazy(func);
+            var threads = new Thread[n];
+            var results = new int[n];
+            for (var i = 0; i < n; ++i)
+            {
+                var current = i;
+                threads[i] = new Thread(() => results[current] = lazy.Get());
+            }
+
+            for (var i = 0; i < n; ++i)
+            {
+                threads[i].Start();
+            }
+
+            for (var i = 0; i < n; ++i)
+            {
+                threads[i].Join();
+            }
+
+            for (var i = 0; i < n - 1; ++i)
+            {
+                Assert.IsTrue(results[i].Equals(results[i + 1]));
+            }
         }
     }
 }
