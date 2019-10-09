bdaacf0bc3934eafd5f3847a59ddeb34ecdaf24b
unknown <alnardn207@gmail.com>
2019-10-09 03:24:27 +0300

    organizing my paths

---
+++ Tests/FinalTest1.6/ListMethodsNameSpace/ListMethodsNameSpace/ListMethods.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ListMethodsNameSpace
+{
+    /// <summary>
+    /// Class for method of counting zero elements.
+    /// </summary>
+    /// <typeparam name="T">Generic parameter.</typeparam>
+    public static class ListMethods<T>
+    {
+        public static int CountOfZeroElements(List<T> list, INullElement<T> nullElement)
+        {
+            if (list == null || nullElement == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            int count = 0;
+            foreach (var item in list)
+            {
+                if (nullElement.IsNull(item))
+                {
+                    ++count;
+                }
+            }
+            return count;
+        }
+    }
+}
