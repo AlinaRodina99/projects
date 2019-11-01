fbe38d0b8cd8624a7e7945952ed998d0e90653d6
alnardn207 <alnardn207@gmail.com>
2019-11-01 15:19:10 +0300

    added test1

---
+++ homework.sem3/Tests/Test1/ServerChat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace ServerChat
+{
+    class Program
+    {
+        static Server server; // сервер
+        static Thread listenThread; // потока для прослушивания
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите порт: ");
+                var port = Convert.ToInt32(Console.ReadLine());
+                server = new Server(port);
+                listenThread = new Thread(server.Listen);
+                listenThread.Start(); //старт потока
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ homework.sem3/Tests/Test1/ServerChat/Server.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServerChat
+{
+    public class Server
+    {
+        private static TcpListener tcpListener; // сервер для прослушивания
+        private static TcpClient tcpClient = tcpListener.AcceptTcpClient();
+        private Client client;
+        private int port;
+
+        public Server(int port)
+        {
+            this.port = port;
+        }
+
+        protected internal void AddConnection(Client client)
+        {
+            this.client = client;
+        }
+
+        // прослушивание входящих подключений
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(port);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Thread clientThread = new Thread(new ThreadStart(client.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            if (message == "exit")
+            {
+                Disconnect();
+            }
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            client.Stream.Write(data, 0, data.Length); //передача данных
+        }
+
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop(); //остановка сервера
+            client.Close();
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+}
+
