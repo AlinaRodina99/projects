1b399ccc6d1bf701e9392f3dc17f161ec6329b5e
alnardn207 <alnardn207@gmail.com>
2019-12-06 15:42:07 +0300

    added test2

---
+++ homework.sem3/Tests/TicTacToe_test2/TicTacToe_test2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TicTacToe_test2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GameBoard MyGameBoard = new GameBoard();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = MyGameBoard;
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {//Updates UI and calls gameBoard update
+            var clickedButton = sender as Button;
+
+            if (MyGameBoard.currentPlayer == GameBoard.CurrentPlayer.X)
+            {
+                clickedButton.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#811717"));
+            }
+            else if (MyGameBoard.currentPlayer == GameBoard.CurrentPlayer.O)
+            {
+                clickedButton.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#126712"));
+            }
+            clickedButton.Background = Brushes.WhiteSmoke;
+            clickedButton.Content = MyGameBoard.currentPlayer;
+            clickedButton.IsHitTestVisible = false;
+
+            MyGameBoard.UpdateBoard(clickedButton.Name);
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {//Restarts Game
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(MyGrid) - 1; i++) // This loop iterates through all the buttons/tiles in the grid and sets changed properties to default
+            {
+                var child = VisualTreeHelper.GetChild(MyGrid, i) as Button;
+                child.Content = null;
+                child.IsHitTestVisible = true;
+                child.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFDDDDDD"));
+            }
+            MyGameBoard = new GameBoard();
+            DataContext = MyGameBoard;
+        }
+    }
+}
